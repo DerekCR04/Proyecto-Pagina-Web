c97af470022ea6e05ea93a952c9239f1d9ebb14a
User <User@DESKTOP-V666RUJ>
2023-04-02 12:26:56 -0600

    Inicio del proyecto

---
+++ src/test/java/com/ProyectoPaginaWeb/ProyectoPaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoPaginaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
