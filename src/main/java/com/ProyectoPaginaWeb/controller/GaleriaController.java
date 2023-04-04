030210293776176b56d95d2eb0b9a59aa0c042f9
Jonathan Esquivel G <jona.esquivel.g@gmail.com>
2023-04-04 12:30:27 -0600

    Avance proyecto Jonathan página Galeria Fotos

---
+++ src/main/java/com/ProyectoPaginaWeb/controller/GaleriaController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.ProyectoPaginaWeb.controller;
+
+import com.ProyectoPaginaWeb.domain.Galeria;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Slf4j
+@Controller
+public class GaleriaController {
+    
+    @GetMapping("/galeria")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "/fotos";
+    }
+    
+    @GetMapping("/galeria/fotos")
+    public String Galeria(Galeria galeria){
+        return "/galeria/fotos";
+    }
+    
+}
