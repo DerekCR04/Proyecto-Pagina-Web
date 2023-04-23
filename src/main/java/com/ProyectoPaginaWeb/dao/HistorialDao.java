e1543cae4866e08a57685d5fe185b0e100418745
Jonathan Esquivel G <jona.esquivel.g@gmail.com>
2023-04-23 17:37:20 -0600

    Trabajo en Detalles del Sitio web

--- src/main/java/com/ProyectoPaginaWeb/dao/HistorialDao.java
+++ src/main/java/com/ProyectoPaginaWeb/dao/HistorialDao.java
@@ -2,6 +2,7 @@
 package com.ProyectoPaginaWeb.dao;
 
 import com.ProyectoPaginaWeb.domain.Historial;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -9,5 +10,9 @@ import org.springframework.data.repository.CrudRepository;
  * @author leona
  */
 public interface HistorialDao extends CrudRepository<Historial, Long> {
+ // List<Historial> findByIdCliente(Long idCliente);
     
+   // HistorialDetalle findByIdCliente(Long idCliente, Historial historial);
+    
+   // void deleteByIdCliente(Long idCliente);  
 }
--- src/main/java/com/ProyectoPaginaWeb/service/AgendaService.java
+++ src/main/java/com/ProyectoPaginaWeb/service/AgendaService.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.ProyectoPaginaWeb.service;
 
 import com.ProyectoPaginaWeb.domain.Agenda;
 import java.util.List;
 
-/**
- *
- * @author User
- */
+
 public interface AgendaService {
     
     public List<Agenda> getAgenda();
